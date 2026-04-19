94f9d60b17f185c7c052bd4fcfae33d487349dc5
gabalingog <gabriellealingog@gmail.com>
2026-04-19 16:09:56 -0400

    video on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ export default function App() {
     <div className="app">
       <Navbar />
       <div className="app-content">
+        <section className="video">
+          <video autoPlay muted loop playsInline className="video-video">
+            <source src="/video.mov" type="video/mp4" />
+          </video>
+          <div className="video-overlay" />
+          <div className="video-text">
+            <p className="video-first">Formula 1</p>
+            <p className="video-tagline">THE BEST OR NOTHING</p>
+            <h1 className="video-title">Mercedes</h1>
+          </div>
+        </section>
         <section id="overview" className="section">
           <h1>Overview</h1>
         </section>
